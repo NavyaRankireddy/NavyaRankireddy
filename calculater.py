a1be825d0eb139927ef09cb3849070e24929a5cb
Rankireddy Navya Satya Sai Eswari <navyarankireddy11@gmail.com>
2024-06-30 19:42:12 +0530

    task2

---
+++ calculater.py
@@ -0,0 +1,51 @@
+# Define a function for each operation
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero"
+    else:
+        return x / y
+
+# Function to display the calculator menu and perform operations
+def calculator():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print("Invalid input")
+
+# Main program loop
+while True:
+    calculator()
+    again = input("Do you want to calculate again? (yes/no): ").lower()
+    if again != "yes":
+        break
